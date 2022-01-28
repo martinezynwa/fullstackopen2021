a7b0c72cd9db1ed5e86fcefff79dac8ac92e5a0c
martinezynwa <martinmgs@seznam.cz>
2022-01-28 09:32:31 +0100

    1.4: course information step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,29 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+	const course = 'Half Stack application development'
+	const parts = [
+		{
+		  name: 'Fundamentals of React',
+		  exercises: 10
+		},
+		{
+		  name: 'Using props to pass data',
+		  exercises: 7
+		},
+		{
+		  name: 'State of a component',
+		  exercises: 14
+		}
+	]
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>{part1.name}: {part1.exercises}</p>
-      <p>{part2.name}: {part2.exercises}</p>
-      <p>{part3.name}: {part3.exercises}</p>
-      <p>Number of exercises: {part1.exercises + part2.exercises + part3.exercises}</p>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total parts={parts} />
+		</div>
+	)
+};
+
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+	)
+};
+
+const Content = (props) => {
+	const lists = props.parts.map(function(item) {
+		return (
+			<div>
+				<p> {item.name}: {item.exercises}</p>
+			</div>
+		)
+	})
+	
+	return lists
+};
+
+const Total = (props) => {
+	var score = 0
+	
+	const lists = props.parts.map(function(item) {
+		
+		score = score + item.exercises 
+		
+	})
+	
+	return (
+		<div>
+			<p>Number of exercises: {score} </p>
+		</div>
+	)
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));

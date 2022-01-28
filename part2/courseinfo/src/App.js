e305a0659a2068404f860ab54633916631cad719
martinezynwa <martinmgs@seznam.cz>
2022-01-28 10:09:26 +0100

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,66 +1,58 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Course from "./Course";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filteredName, setFilteredName] = useState('')
-
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newNumber
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-    } else {
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const handleNameAddition = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberAddition = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFiltering = (event) => {
-    setFilteredName(event.target.value)
-  }
-
-  const personsToShow = filteredName === ''
-    ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(filteredName.toLowerCase()))
+  ]
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <div>Filter names: <input value={filteredName} onChange={handleFiltering} /></div>
-      <form onSubmit={addName}>
-        <div>name: <input value={newName} onChange={handleNameAddition} /></div>
-        <div>number: <input value={newNumber} onChange={handleNumberAddition} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {personsToShow.map((person, index) => <div key={index}>{person.name} {person.number}</div>)}
-      </div>
+      <h1>Web development curriculum</h1>
+      <Course course={courses} />
     </div>
   )
 }
-
 
 export default App

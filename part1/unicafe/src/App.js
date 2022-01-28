a908c553821ae530e2a4d3715323a9b0cb833bc7
martinezynwa <martinmgs@seznam.cz>
2022-01-28 09:39:05 +0100

    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,34 +1,41 @@
 import React, { useState } from 'react'
 
-const Statistics = ({ good, neutral, bad }) => {
-    return (
-        <div>
-            <p>{console.log(good, neutral, bad)}</p>
-            <h1>Statistics</h1>
-            <div>good {good}</div>
-            <div>neutral {neutral}</div>
-            <div>bad {bad}</div>
-            <div>all {good + neutral + bad}</div>
-            <div>Average: {(good - bad) / (good + neutral + bad)}</div>
-            <div>Positive: {((good) / (good + neutral + bad)) * 100}%</div>
-        </div>
-    )
-}
 
 /* const Statistics = (props) => {
-  return (
-    <div>
-      <p>{console.log(props.good, props.neutral, props.bad)}</p>
-      <h1>Statistics</h1>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
-      <div>all {props.good + props.neutral + props.bad}</div>
-      <div>Average: {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</div>
-      <div>Positive: {((props.good) / (props.good + props.neutral + props.bad))*100}%</div>
-    </div>
-  )
-} */
+  if (props.All === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <div>no feedback given</div>
+      </div>
+    )
+  } */
+
+const Statistics = ({ good, neutral, bad }) => {
+
+    if (good === 0 && neutral === 0 && bad === 0) {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <div>no feedback given</div>
+            </div>
+        )
+    }
+
+    else {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <div>good {good}</div>
+                <div>neutral {neutral}</div>
+                <div>bad {bad}</div>
+                <div>all {good + neutral + bad}</div>
+                <div>Average: {(good - bad) / (good + neutral + bad)}</div>
+                <div>Positive: {((good) / (good + neutral + bad)) * 100}%</div>
+            </div>
+        )
+    }
+}
 
 const App = () => {
     const [good, setGood] = useState(0)

0c2e081783b6c313f3f3cef7207ec4ae7e578d61
martinezynwa <martin.gnebus@gmail.com>
2022-02-07 12:11:58 +0100

    4.5*: helper functions and unit tests, step1-3

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,26 @@
+const dummy = () => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (previous, current) => (previous.likes > current.likes ? previous : current),
+    {}
+  )
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+}

297179760a516e8ed3115269462fac16a812eff8
martinezynwa <martin.gnebus@gmail.com>
2022-02-11 22:11:34 +0100

    5.2: bloglist frontend, step1-2

---
+++ part5/blog/src/App.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(initialBlogs => {
+      setBlogs(initialBlogs)
+    })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h1>Log-in</h1>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const logoutHandler = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <h1>Blogs</h1>
+          <p>{user.name} logged-in</p>
+          <button
+            onClick={() => {
+              logoutHandler()
+            }}
+          >
+            logout
+          </button>
+          <ul>
+            {blogs.map(blog => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App

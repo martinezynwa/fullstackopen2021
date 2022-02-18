e00d1488a0b7f6cacd690d52c569652954906af6
martinezynwa <martin.gnebus@gmail.com>
2022-02-18 14:00:18 +0100

    6.14 Anecdotes and the backend, step1-2

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
-const App = () => (
-  <div>
-    <Notification />
-    <Filter />
-    <AnecdoteForm />
-    <AnecdoteList />
-  </div>
-)
+const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(a => dispatch(setAnecdotes(a)))
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <Notification />
+      <Filter />
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
 
 export default App
--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,28 +1,9 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
+    case 'ALL_ANECDOTES':
+      return action.data
     case 'NEW_ANECDOTE':
-      return [...state, asObject(action.data)]
+      return [...state, action.data]
     case 'VOTE':
       return state.map(s =>
         s.id === action.data ? { ...s, votes: s.votes + 1 } : s,
@@ -40,6 +21,11 @@ export const createAnecdote = content => ({
 export const voteFor = id => ({
   type: 'VOTE',
   data: id,
+})
+
+export const setAnecdotes = content => ({
+  type: 'ALL_ANECDOTES',
+  data: content,
 })
 
 export default reducer
